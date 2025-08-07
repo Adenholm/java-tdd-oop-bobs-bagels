4b97726261a37f741015a9748e48340d37ca7cde
Hanna Adenholm <hanna.adenholm@se.experis.com>
2025-08-07 16:32:18 +0200

    Added new tests

--- src/test/java/com/booleanuk/core/BagelShopTest.java
+++ src/test/java/com/booleanuk/core/BagelShopTest.java
@@ -19,4 +19,26 @@ public class BagelShopTest {
         Assertions.assertTrue(basket.remove("BGLO"));
         Assertions.assertTrue(basket.getItems().isEmpty());
     }
+
+    @Test
+    public void testAddingUntilFull(){
+        Basket basket = new Basket();
+        Assertions.assertEquals(1, basket.add("BGLO"));
+        Assertions.assertEquals(1, basket.add("BGLO"));
+        Assertions.assertEquals(1, basket.add("BGLO"));
+        Assertions.assertEquals(1, basket.add("BGLO"));
+        Assertions.assertEquals(1, basket.add("BGLO"));
+        Assertions.assertEquals(0, basket.add("BGLO"));
+        Assertions.assertEquals(5, basket.getItems().size());
+    }
+
+    @Test
+    public void testChangeBasketStdCapacity(){
+        Basket basket = new Basket();
+        Assertions.assertEquals(5, basket.getCapacity());
+        Basket.setStdCapacity(3);
+        Assertions.assertEquals(3, Basket.getStdCapacity());
+        Basket basket2 = new Basket();
+        Assertions.assertEquals(3, basket2.getCapacity());
+    }
 }
